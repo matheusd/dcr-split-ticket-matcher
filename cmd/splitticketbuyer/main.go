14abfcfaf368b337b43705a5d961cf181d30c140
Matheus Degiovani <opensource@matheusd.com>
2018-05-08 17:49:10 -0300

    Small improvement in cli config init

--- cmd/splitticketbuyer/main.go
+++ cmd/splitticketbuyer/main.go
@@ -19,12 +19,22 @@ func main() {
 	fmt.Printf("Split ticket buyer version %s\n", pkg.Version)
 
 	if !buyer.DefaultConfigFileExists() {
-		fmt.Println("Initializing buyer config based on existing dcrwallet.conf")
+		fmt.Println("Default config file does not exist. Initializing buyer config based on existing dcrwallet.conf")
 		err := buyer.InitConfigFromDcrwallet()
 		if err != nil {
 			fmt.Println(err)
-			os.Exit(1)
+		} else {
+			cfg, err := buyer.LoadConfig()
+			if err == nil {
+				fmt.Printf("Initialized config file to %s\n", cfg.ConfigFile)
+				err = cfg.Validate()
+				if err != nil {
+					fmt.Printf("Config not yet ready: %s\n", err)
+					fmt.Printf("Please edit and complete the config file as needed.")
+				}
+			}
 		}
+		os.Exit(1)
 	}
 
 	reporter := buyer.NewWriterReporter(os.Stdout)
