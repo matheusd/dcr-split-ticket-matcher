4a66c0270f50b91e611dfc7b88f9ce9586170d95
Matheus Degiovani <opensource@matheusd.com>
2018-08-22 15:24:18 -0300

    Version bump

--- pkg/version.go
+++ pkg/version.go
@@ -16,9 +16,9 @@ const (
 const (
 	MajorVersion = 0
 	MinorVersion = 6
-	PatchVersion = 0
+	PatchVersion = 1
 )
 
 // Version is the package version. Remember to modify the previous constants as
 // well on version bumps.
-const Version = "0.6.0"
+const Version = "0.6.1+pre"
