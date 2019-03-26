2feb99d53281ec0419809661d6de787aabf2cf03
Matheus Degiovani <opensource@matheusd.com>
2019-03-26 10:04:46 -0300

    buyer: Don't duplicate TxIn
    This removes the duplication of the TxIn that stores split inputs, as it
    is uneeded.

--- pkg/buyer/wallet-client.go
+++ pkg/buyer/wallet-client.go
@@ -269,8 +269,7 @@ func (wc *WalletClient) generateSplitTxInputs(ctx context.Context, session *Sess
 
 	session.splitInputs = make([]*wire.TxIn, len(tx.TxIn))
 	for i, in := range tx.TxIn {
-		outp := wire.NewOutPoint(&in.PreviousOutPoint.Hash, in.PreviousOutPoint.Index, in.PreviousOutPoint.Tree)
-		session.splitInputs[i] = wire.NewTxIn(outp, wire.NullValueIn, nil)
+		session.splitInputs[i] = in
 	}
 
 	return nil
